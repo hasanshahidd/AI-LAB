b6425dac327b6026dcb1a13774f0f2a9528ffcde
Hassan <148881982+hasanshahidd@users.noreply.github.com>
2024-12-05 19:48:19 +0500

    lab 9.py

--- lab tast 9/lab 9.py
+++ lab tast 9/lab 9.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 netflix_data = pd.read_csv(r"C:\Users\Admin\Desktop\AI LAB 9 TO 14\lab tast 9\netflix_titles.csv")
-
 print("First few rows of the dataset:")
 print(netflix_data.head())
 print("\nSummary of the dataset:")
@@ -20,7 +19,6 @@ genres_list = netflix_data['listed_in'].str.split(', ')
 genres_list = genres_list.explode()
 print("\nTop 10 Genres:")
 print(genres_list.value_counts().head(10))
-
 sns.set(style='whitegrid')
 plt.figure(figsize=(8, 6))
 sns.countplot(x='type', data=netflix_data, palette='viridis')
@@ -28,14 +26,12 @@ plt.title('Distribution of Movies and TV Shows')
 plt.xlabel('Type')
 plt.ylabel('Count')
 plt.show()
-
 plt.figure(figsize=(12, 6))
 sns.countplot(x='rating', data=netflix_data, palette='magma', order=netflix_data['rating'].value_counts().index)
 plt.title('Distribution of Ratings')
 plt.xlabel('Rating')
 plt.ylabel('Count')
 plt.show()
-
 plt.figure(figsize=(12, 8))
 top_genres = genres_list.value_counts().head(10)
 sns.barplot(x=top_genres.values, y=top_genres.index, palette='cubehelix')
@@ -43,6 +39,5 @@ plt.title('Top 10 Genres')
 plt.xlabel('Count')
 plt.ylabel('Genre')
 plt.show()
-
 netflix_data.to_csv('cleaned_netflix_titles.csv', index=False)
 print("\nAll tasks completed successfully!")
